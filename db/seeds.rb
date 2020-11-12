5210a3abd51f33f87d6d7e0d4264292e071a1a42
Gaelle Shearn <gaelle.shearn.jmr@gmail.com>
2020-11-12 08:51:04 +0100

    coucou toi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+caperliparlacoloc = { name: "Caperliparlacoloc", address: "LV Lyon", description: "Cute & pretty", price_per_night: 50, number_of_guest: 3 }
+montribloc = { name: "Montribloc", address: "M Lyon", description: "Big & spacious", price_per_night: 70, number_of_guest: 12 }
+
+[ caperliparlacoloc, montribloc ].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
